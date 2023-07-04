77ce5d7a59444f23006a60d5fabb179e8abf426a
venkatesh <venki11112222@gmail.com>
2023-07-03 17:05:38 -0700

    init

--- sample.py
+++ sample.py
@@ -1,3 +1,4 @@
 print("Venkat is a good boy and he is a nice boy.")
 print("sample file running and good to use this one.")
 print("Staying near anjaneya swamy temple")
+print("I commit and push the code using VS Code")
